836b223e68a576e50e3a14a1a0158f4ca88b06d9
unknown <313039228@qq.com>
2025-01-04 20:10:50 +0800

    init

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const parseColor = (color: string) => {
+  const hex = color.startsWith("#") ? color.slice(1) : color
+  return parseInt(hex, 16)
+}
